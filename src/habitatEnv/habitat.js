a8ff9352a93996c7e93f1446bf7dd9819f4806c7
Herman Rogers <herman.rogers@gmail.com>
2018-04-26 15:54:58 -0400

    Add env support for web and react native

--- src/habitatEnv/habitat.js
+++ src/habitatEnv/habitat.js
@@ -1,77 +1,16 @@
-let fs = Object.create(null);
-try {
-  // eslint-disable-next-line global-require
-  fs = require('fs');
-} catch (e) {
-  // eslint-disable-next-line no-console
-  console.log('could not detect fs, running outside node env.');
-}
-
-const fileName = '.env.json';
-
-export function traversePath(path) {
-  const files = fs.readdirSync(path);
-  const envFile = [];
-
-  files.forEach((file) => {
-    if (file === fileName) {
-      envFile.push(file);
-    }
-  });
-
-  return envFile[0];
-}
-
-export function getJSONConfig(directory) {
-  const isWindows = process.platform === 'win32';
-  const lineEndings = isWindows ? '\\' : '/';
-  const searchDir = directory || __dirname;
-  const splitDir = searchDir.split(lineEndings);
-
-  let file = null;
-
-  for (let i = splitDir.length - 1; i > 0; i -= 1) {
-    const path = splitDir.join(lineEndings);
-    file = traversePath(path);
-
-    if (file != null) break;
-    splitDir.splice(i);
-  }
-
-  if (file) {
-    const content = JSON.parse(fs.readFileSync(file, 'utf8'));
-    return content;
-  }
-  return null;
-}
-
-// Expects a .json config
 export function setupEnvironment(config) {
-  global.habitat = {};
+  const { env } = config;
 
-  switch (config.env) {
-    case 'development':
-      global.habitat = config.development;
-      break;
-    case 'staging':
-      global.habitat = config.staging;
-      break;
-    case 'production':
-      global.habitat = config.production;
-      break;
-    default:
-      global.habitat = {};
-  }
+  global.habitat = config[env];
   global.habitat.env = config.env;
 }
 
 export function create(config) {
-  if (!config && !fs) {
+  if (!config) {
+    const warning = 'Imported .env.json file is required to create habitat';
     // eslint-disable-next-line no-console
-    console.warning('Detected non-nodejs env, please pass in .env.json manually');
-    return;
+    throw new Error(warning);
   }
-  const loadedEnv = config || getJSONConfig();
-  setupEnvironment(loadedEnv);
+  setupEnvironment(config);
 }
 
--- src/habitatEnv/habitat.test.js
+++ src/habitatEnv/habitat.test.js
@@ -11,44 +11,23 @@ describe('habitat', () => {
       TEST_API: 'www.example.com',
     },
   };
-  const mockFileInfo = {
-    '/mock/env/.env.json': mockConfig,
-  };
-
-  beforeEach(() => {
-    fs.setMockFiles(mockFileInfo);
-  });
 
   afterEach(() => {
     global.habitat = null;
   });
 
-  it('should traverse a list of files to find specified env files', () => {
-    expect(habitat.getJSONConfig('/mock/env')).toEqual(mockFileInfo['/mock/env/.env.json']);
-  });
-
   it('should build the environment with .env file', () => {
-    habitat.setupEnvironment(habitat.getJSONConfig('/mock/env'));
+    habitat.setupEnvironment(mockConfig);
 
     expect(global.habitat.TEST_API).toEqual('www.example.com');
   });
 
   it('should place the env specification into the global namespace', () => {
-    habitat.setupEnvironment(habitat.getJSONConfig('/mock/env'));
+    habitat.setupEnvironment(mockConfig);
     expect(global.habitat.env).toEqual('development');
   });
 
-  it('should allow us to call create and inject a config file', () => {
-    habitat.create(mockConfig);
-
-    expect(global.habitat.env).toEqual('development');
-  });
-
-  it('should allow habitat to work in non-node environments', () => {
-    fs.readdirSync = null;
-    fs.readFileSync = null;
-    habitat.create(mockConfig);
-
-    expect(global.habitat.env).toEqual('development');
+  it('should return a error if no config file is passed', () => {
+    expect(() => { habitat.create(); }).toThrow(new Error('Imported .env.json file is required to create habitat'));
   });
 });
